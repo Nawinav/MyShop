62a5b615dbc7f96d44338cdeeeca489e7c469ee4
Nawinav <avnaveenkumar16@gmail.com>
2020-02-17 14:35:45 +0530

    Initial Slotion Setup

---
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.DataAccess;
+using MyShopCore;
+using MyShop.WebUI;
+using MyShop.Services;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
