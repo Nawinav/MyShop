8de4ddde7f4ce30df7f25c2cb65affea5e250557
Nawinav <avnaveenkumar16@gmail.com>
2020-02-17 14:35:45 +0530

    Initial Slotion Setup

---
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.DataAccess.InMemory;
+using MyShop.DataAccess;
+using MyShopCore;
+using MyShop.WebUI;
+using MyShopCore.Models;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        // GET: ProductManager
+         ProductRepository context;
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.commit();
+                return RedirectToAction("Index");
+            }
+
+        }
+        public ActionResult Edit(string ID)
+        {
+            Product product = context.Find(ID);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product,string ID)
+        {
+            Product productToEdit=context.Find(ID);
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+
+
+                productToEdit.Category = product.Category;
+                productToEdit.Description = product.Description;
+                productToEdit.Image = product.Image;
+                productToEdit.Name = product.Name;
+                productToEdit.Price = product.Price;
+                context.commit();
+                return RedirectToAction("Index");
+                }
+            }
+        public ActionResult Delete(string ID)
+        {
+
+            Product productToDelete = context.Find(ID);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productToDelete);
+
+            }
+
+        }
+        [HttpPost]
+        [ActionName("DeleteCustomer")]
+        public ActionResult confirmDelete(string ID)
+        {
+            Product productToDelete = context.Find(ID);
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(ID);
+                context.commit();
+                return RedirectToAction("Index");
+
+            }
+        }
+
+
+
+        }
+        
+       
+   
+
+    }
